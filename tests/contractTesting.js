727ac4431ff9d1beace5a4aafb4ab3413b86e5f4
Papa Goose <papa.goose@koralabs.io>
2023-02-23 08:27:47 -0800

    Better testing logging

--- tests/contractTesting.js
+++ tests/contractTesting.js
@@ -25,12 +25,22 @@ export async function testSuccess(testName, paramNames) {
     });
 }
 
-export async function testFailure(testName, paramNames) {
+export async function testFailure(testName, paramNames, message=null) {
     const args = paramNames.map((p) => _program.evalParam(p));
     _contract.runWithPrint(args).then((res) => {
         const assertion = res[0].toString() != "()";
         if (assertion) {
-          console.log(`${Color.FgGreen}Test ${testName} was successful!${Color.Reset}`);
+          if (message) {
+            if (res[1][0] == message) {
+              console.log(`${Color.FgGreen}Test ${testName} was successful!${Color.Reset}`);
+            }
+            else {
+              logFail(testName, res, args);
+            }
+          }
+          else {
+            console.log(`${Color.FgGreen}Test ${testName} was successful!${Color.Reset}`);
+          }
         } 
         else {
             logFail(testName, res, args);
@@ -41,11 +51,22 @@ export async function testFailure(testName, paramNames) {
       })
 }
 
-export async function logFail(testName, obj, args, isError=false) {
+export async function logFail(testName, obj, args, isRuntimeError=false) {
     console.log(`${Color.FgRed}Test ${testName} failed!${Color.Reset}`);
-    console.log(`${Color.FgRed}--------------${Color.Reset}`)
-    if (isError) console.log("  *ERROR*");
-    console.log(`   ${Color.FgRed}ARGS: ${Color.Reset}`, args.map((v) => v.toString()));
-    console.log(`   ${Color.FgRed}${obj}${Color.Reset}`);
-    console.log(`${Color.FgRed}--------------${Color.Reset}`)
+    console.log(`${Color.FgRed}------------------------------${Color.Reset}`)
+    let message = obj
+    console.log(`   ${Color.FgYellow}ARGS:${Color.Reset}`, args.map((v) => v.toString()));
+    if (isRuntimeError) {
+      console.log("   **RUNTIME ERROR**");
+    }
+    else {
+      console.log(`   ${Color.FgYellow}MESSAGE:${Color.Reset}`);
+      if (Array.isArray(obj)){
+        console.log(obj[0]);
+        if (obj.length > 1) {
+          console.log(`\n\n   ${Color.FgYellow}PRINT STATEMENTS:${Color.Reset}\n   ${obj[1].join("\n   ")}`);
+        }
+      }
+    }
+    console.log(`${Color.FgRed}------------------------------${Color.Reset}`)
 }
