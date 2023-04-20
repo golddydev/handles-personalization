3b00c3288ac3cf9e7ff3388667cdb6d1c66b06b5
Papa Goose <papa.goose@koralabs.io>
2023-04-20 13:23:10 -0700

    Adding checks for IPFS file validation

--- tests/contractTesting.js
+++ tests/contractTesting.js
@@ -15,83 +15,52 @@ export function setup(program, contract) {
 }
 
 // evalParam(p) and runWithPrint(p[]). Look for unit "()" response
-export async function testApproval(testGroup, testName, paramNames) {
+export async function testCase(shouldApprove, testGroup, testName, paramNames, message=null) {
   testCount++;
-  const args = paramNames.map((p) => _program.evalParam(p));
-  await _contract.runWithPrint(args).then((res) => {
-      const assertion = res[0].toString() == "()";
-      //console.log(res[1]);
-      if (assertion) {
-        successCount++
-        console.log(`${Color.FgGreen}*success* - APPROVE - ${testGroup.padEnd(25)} '${testName}'${Color.Reset}`);
-      } else {
-        logFail(testGroup, testName, res, args, 'APPROVE');
-      }
-    })
-    .catch((err) => {
-      logFail(testGroup, testName, err, args, 'APPROVE', true);
-    });
+  const args = arguments;
+  const params = paramNames.map((p) => _program.evalParam(p));
+  await _contract.runWithPrint(params).then((res) => {
+    logTest(args[0], args[1], args[2], args[3], args.length == 5 ? args[4] : null, res);
+  })
+  .catch((err) => {
+    logTest(args[0], args[1], args[2], args[3], args.length == 5 ? args[4] : null, err);
+  });
 }
 
-export async function testDenial(testGroup, testName, paramNames, message=null) {
-    testCount++;
-    const args = paramNames.map((p) => _program.evalParam(p));
-    await _contract.runWithPrint(args).then((res) => {
-        const assertion = res[0].toString() != "()";
-        //console.log(res[1]);
-        if (assertion) {
-          if (message) {
-            if (res[1][0] == message) {
-              successCount++
-              console.log(`${Color.FgGreen}*success* - DENY    - ${testGroup.padEnd(25)} '${testName}'${Color.Reset}`);
-            }
-            else {
-              logFail(testGroup, testName, res, args, 'DENY', false, message);
-            }
-          }
-          else {
-            successCount++
-            console.log(`${Color.FgGreen}*success* - DENY    - ${testGroup.padEnd(25)} '${testName}'${Color.Reset}`);
-          }
-        } 
-        else {
-            logFail(testGroup, testName, res, args, 'DENY');
-        }
-      })
-      .catch((err) => {
-        logFail(testGroup, testName, err, args, 'DENY', true);
-      })
-}
-
-function logFail(testGroup, testName, obj, args, type, isCodeError=false, message=null) {
+function logTest(shouldApprove, testGroup, testName, paramNames, message=null, res) {
+  const hasPrintStatements = Array.isArray(res) && res.length > 1 && res[1].length > 1;
+  const assertion = Array.isArray(res) && (shouldApprove ? res[0].toString() == "()" : res[0].toString() != "()" && (!message || res[1][0] == message));
+  const textColor = assertion ? Color.FgGreen : Color.FgRed
+  
+  if (!assertion || hasPrintStatements)
+    console.log(`${textColor}------------------------------${Color.Reset}`)
+  
+  console.log(`${textColor}*${assertion ? "succcess" : "failure"}* - ${(shouldApprove ? "APPROVE" : "DENY").padEnd(7)} - ${testGroup.padEnd(25)} '${testName}'${Color.Reset}`);
+  
+  if (hasPrintStatements)
+    console.log(`   ${Color.FgYellow}PRINT STATEMENTS:${Color.Reset}\n   ${res[1].join("\n   ")}`);
+  
+  if (assertion) {
+    successCount++
+  }
+  else {
     failCount++
-    console.log(`${Color.FgRed}*failure* - ${type.padEnd(7)} - ${testGroup.padEnd(25)} '${testName}'${Color.Reset}`);
-    console.log(`${Color.FgRed}------------------------------${Color.Reset}`)
-    // console.log(`   ${Color.FgYellow}ARGS:${Color.Reset}`, args.map((v) => v.toString()));
-    if (isCodeError) {
-      console.log(`   ${Color.FgRed}**RUNTIME ERROR**${Color.Reset}`);
-    }
     console.log(`   ${Color.FgYellow}MESSAGE:${Color.Reset}`);
-    if (Array.isArray(obj)){
-      console.log(obj[0]);
-      console.log(`\n`);
-      if (message) {
-        console.log(`   ${Color.FgYellow}EXPECTED:\n   ${Color.FgBlue}${message}${Color.Reset}`);
-        if (obj.length > 1) {
-          // Helios error() is always the last in the output/print statements obj[1].length-1]
-          console.log(`   ${Color.FgYellow}RECEIVED:\n   ${Color.FgRed}${obj[1][obj[1].length-1]}${Color.Reset}`);
-        }
+    if (Array.isArray(res))
+      console.log(res[0]);
+      console.log(`\n`)
+      console.log(`   ${Color.FgYellow}EXPECTED:\n   ${Color.FgBlue}${message ? messsage : "success"}${Color.Reset}`);
+      if (res.length > 1) {
+        // Helios error() is always the last in the output/print statements res[1].length-1]
+        console.log(`   ${Color.FgYellow}RECEIVED:\n   ${Color.FgRed}${res[1][res[1].length-1]}${Color.Reset}`);
       }
-      else {
-        if (obj.length > 1) {
-          console.log(`   ${Color.FgYellow}PRINT STATEMENTS:${Color.Reset}\n   ${obj[1].join("\n   ")}`);
-        }
-      }
-    }
     else {
-      console.log(obj);
+      console.log(res);
     }
-    console.log(`${Color.FgRed}------------------------------${Color.Reset}`)
+  }
+  
+  if (!assertion || hasPrintStatements)
+    console.log(`${textColor}------------------------------${Color.Reset}`)
 }
 
 export function displayStats() {
--- tests/tests.js
+++ tests/tests.js
@@ -14,35 +14,36 @@ tester.setup(program, testContract);
 Promise.all([
 
     // SHOULD APPROVE
-    tester.testApproval("UPDATE_NFT_HANDLE", "happy path", ["good_datum", "update_nft_redeemer_good", "good_default"]),
-    tester.testApproval("UPDATE_NFT_HANDLE", "partner pfp/bg lists", ["good_datum", "update_nft_redeemer_good", "good_ref_sub"]),
+    tester.testCase(true, "UPDATE_NFT_HANDLE", "happy path", ["good_datum", "update_nft_redeemer_good", "good_default"]),
+    tester.testCase(true, "UPDATE_NFT_HANDLE", "partner pfp/bg lists", ["good_datum", "update_nft_redeemer_good", "good_ref_sub"]),
 
-    tester.testApproval("ADMIN_UPDATE", "happy path", ["good_datum", "good_admin_redeemer", "good_admin_ctx"]),
+    tester.testCase(true, "ADMIN_UPDATE", "happy path", ["good_datum", "good_admin_redeemer", "good_admin_ctx"]),
     
     // SHOULD DENY
-    tester.testDenial("UPDATE_NFT_HANDLE", "handle missing from inputs", ["good_datum", "update_nft_redeemer_good", "handle_missing_from_inputs"], "Handle is not in input list"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "bad user token name", ["good_datum", "update_nft_redeemer_good", "wrong_handle_name"], "Handle is not in input list"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "bad user token label", ["good_datum", "update_nft_redeemer_good", "wrong_handle_label"], "Handle is not in input list"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "bad user token policy", ["good_datum", "update_nft_redeemer_good", "not_a_real_handle"], "Handle is not in input list"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "bad reference token name", ["good_datum", "update_nft_redeemer_good", "bad_ref_token_name"], "Reference Token is not in input list"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "bad reference token label", ["good_datum", "update_nft_redeemer_good", "bad_ref_token_label"], "Reference Token is not in input list"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "bad reference token address", ["good_datum", "update_nft_redeemer_good", "bad_ref_token_creds"], "Reference Token is not in input list"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "bad reference token policy", ["good_datum", "update_nft_redeemer_good", "bad_ref_token_policy"], "Reference Token is not in input list"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "reference token not output to contract", ["good_datum", "update_nft_redeemer_good", "bad_ref_token_wrong_output"], "Reference Token not returned to contract"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "missing bg reference input", ["good_datum", "update_nft_redeemer_good", "missing_bg_ref_input"], "bg_policy_ids reference input not present or not from valid provider"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "missing pfp reference input", ["good_datum", "update_nft_redeemer_good", "missing_pfp_ref_input"], "pfp_policy_ids reference input not present or not from valid provider"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "missing pz reference input", ["good_datum", "update_nft_redeemer_good", "missing_pz_ref_input"], "pz_settings reference input not present"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "provider fee wrong", ["good_datum", "update_nft_redeemer_good", "provider_fee_wrong"], "Personalization provider not found or fee unpaid"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "provider address wrong", ["good_datum", "update_nft_redeemer_good", "provider_address_wrong"], "Personalization provider not found or fee unpaid"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "provider datum wrong", ["good_datum", "update_nft_redeemer_good", "provider_datum_wrong"], "Personalization provider not found or fee unpaid"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "treasury fee wrong", ["good_datum", "update_nft_redeemer_good", "treasury_fee_wrong"], "Handle treasury fee unpaid"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "treasury address wrong", ["good_datum", "update_nft_redeemer_good", "treasury_address_wrong"], "Handle treasury fee unpaid"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "treasury datum wrong", ["good_datum", "update_nft_redeemer_good", "treasury_datum_wrong"], "Handle treasury fee unpaid"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "pz settings wrong address", ["good_datum", "update_nft_redeemer_good", "pz_settings_wrong_address"], "pz_settings reference input not from ADA Handle"),
-    tester.testDenial("UPDATE_NFT_HANDLE", "wrong contract address", ["good_datum", "update_nft_redeemer_good", "wrong_contract_address"], "Contract not found in valid contracts list"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "bad user token name", ["good_datum", "update_nft_redeemer_good", "wrong_handle_name"], "Handle reference input not present"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "bad user token label", ["good_datum", "update_nft_redeemer_good", "wrong_handle_label"], "Handle reference input not present"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "bad user token policy", ["good_datum", "update_nft_redeemer_good", "not_a_real_handle"], "Handle reference input not present"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "bad reference token name", ["good_datum", "update_nft_redeemer_good", "bad_ref_token_name"], "Reference Token is not in input list"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "bad reference token label", ["good_datum", "update_nft_redeemer_good", "bad_ref_token_label"], "Reference Token is not in input list"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "bad reference token address", ["good_datum", "update_nft_redeemer_good", "bad_ref_token_creds"], "Reference Token is not in input list"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "bad reference token policy", ["good_datum", "update_nft_redeemer_good", "bad_ref_token_policy"], "Reference Token is not in input list"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "reference token not output to contract", ["good_datum", "update_nft_redeemer_good", "bad_ref_token_wrong_output"], "Reference Token not returned to contract"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "missing handle reference input", ["good_datum", "update_nft_redeemer_good", "handle_missing_from_inputs"], "Handle reference input not present"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "missing bg reference input", ["good_datum", "update_nft_redeemer_good", "missing_bg_ref_input"], "bg_policy_ids reference input not present or not from valid provider"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "missing pfp reference input", ["good_datum", "update_nft_redeemer_good", "missing_pfp_ref_input"], "pfp_policy_ids reference input not present or not from valid provider"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "missing pz reference input", ["good_datum", "update_nft_redeemer_good", "missing_pz_ref_input"], "pz_settings reference input not present"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "provider fee wrong", ["good_datum", "update_nft_redeemer_good", "provider_fee_wrong"], "Personalization provider not found or fee unpaid"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "provider address wrong", ["good_datum", "update_nft_redeemer_good", "provider_address_wrong"], "Personalization provider not found or fee unpaid"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "provider datum wrong", ["good_datum", "update_nft_redeemer_good", "provider_datum_wrong"], "Personalization provider not found or fee unpaid"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "treasury fee wrong", ["good_datum", "update_nft_redeemer_good", "treasury_fee_wrong"], "Handle treasury fee unpaid"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "treasury address wrong", ["good_datum", "update_nft_redeemer_good", "treasury_address_wrong"], "Handle treasury fee unpaid"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "treasury datum wrong", ["good_datum", "update_nft_redeemer_good", "treasury_datum_wrong"], "Handle treasury fee unpaid"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "pz settings wrong address", ["good_datum", "update_nft_redeemer_good", "pz_settings_wrong_address"], "pz_settings reference input not from ADA Handle"),
+    tester.testCase(false, "UPDATE_NFT_HANDLE", "wrong contract address", ["good_datum", "update_nft_redeemer_good", "wrong_contract_address"], "Contract not found in valid contracts list"),
+    //tester.testDenial("UPDATE_NFT_HANDLE", "missing reference token in outputs", ["good_datum", "update_nft_redeemer_good", "missing_reference_token"], "Reference Token not found in outputs"),
 
-    tester.testDenial("ADMIN_UPDATE", "wrong admin signer", ["good_datum", "good_admin_redeemer", "wrong_admin_ctx"], "Required admin signer(s) not present"),
-    tester.testDenial("ADMIN_UPDATE", "no admin signers", ["good_datum", "good_admin_redeemer", "no_admin_signers_ctx"], "Required admin signer(s) not present")
+    tester.testCase(false, "ADMIN_UPDATE", "wrong admin signer", ["good_datum", "good_admin_redeemer", "wrong_admin_ctx"], "Required admin signer(s) not present"),
+    tester.testCase(false, "ADMIN_UPDATE", "no admin signers", ["good_datum", "good_admin_redeemer", "no_admin_signers_ctx"], "Required admin signer(s) not present")
 
 ]).then(() => {
     tester.displayStats()
