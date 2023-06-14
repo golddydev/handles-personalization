0cfed83eb07ee58d2d169710c6a53805611b21e6
Papa Goose <papa.goose@koralabs.io>
2023-06-14 12:02:17 -0700

    Reference script automation

--- compile.js
+++ compile.js
@@ -1,13 +1,52 @@
 import * as helios from "@hyperionbt/helios"
 import fs from "fs";
-
+const NETWORK = 'preview';
 let contractHelios = fs.readFileSync("./contract.helios").toString();
 let program = helios.Program.new(contractHelios);
 const contract = program.compile(true);
+//console.log(Buffer.from(contract.toCbor()).toString('hex'));
 const address = helios.Address.fromValidatorHash(contract.validatorHash);
+
 fs.writeFileSync("./contract/contract.cbor", contract.serialize());
 fs.writeFileSync("./contract/contract.addr", address.toBech32());
 fs.writeFileSync("./contract/contract.hash", contract.validatorHash.hex);
+
+
+(async () => {
+    const refScriptAddress = 'addr_test1qr8drf5eur4emcrsanv6ppcf3pywzkzpx3pp2hhr273ddgl34r3hjynmsy2cxpc04a6dkqxcsr29qfl7v9cmrd5mm89q4t0cw0';
+    const POLICY_ID = 'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a';
+    const handle = `000de140${Buffer.from('pzcontract.0001').toString('hex')}`;
+    const tx = new helios.Tx();
+    tx.addInput(new helios.UTxO(
+        helios.TxId.fromHex('1078f6a508bde2344bc3bb369d3a0401035c87eb7cbfbc97a8cb3c1e20e843ea'),
+        BigInt(0),
+        new helios.TxOutput(
+            helios.Address.fromBech32(refScriptAddress),
+            new helios.Value(BigInt(10000000000), new helios.Assets([[POLICY_ID, [[handle, 1]]]]))
+        ))
+    );
+    tx.addInput(new helios.UTxO(
+        helios.TxId.fromHex('1078f6a508bde2344bc3bb369d3a0401035c87eb7cbfbc97a8cb3c1e20e843ea'),
+        BigInt(1),
+        new helios.TxOutput(
+            helios.Address.fromBech32(refScriptAddress),
+            new helios.Value(BigInt(10000000000))
+        ))
+    );
+    tx.addOutput(new helios.TxOutput(
+        helios.Address.fromBech32(refScriptAddress),
+        new helios.Value(BigInt(50000000), new helios.Assets([[POLICY_ID, [[handle, 1]]]])),
+        null,
+        contract
+    ));  
+    const networkParams = new helios.NetworkParams(
+        await fetch(`https://d1t0d7c2nekuk0.cloudfront.net/${NETWORK}.json`).then((response) => response.json())
+    );
+    const txBody = await tx.finalize(networkParams, helios.Address.fromBech32(refScriptAddress));
+    fs.writeFileSync("./contract/tx.body", helios.bytesToHex(txBody.toCbor()));
+})()
+
+
 
 // // Quick temp program to test datum encoding
 // program = helios.Program.new(`spending datum_types
--- scripts/create_reference_script_tx.sh
+++ scripts/create_reference_script_tx.sh
@@ -1,8 +1,8 @@
 cardano-cli transaction build --babbage-era --testnet-magic 2 \
---tx-in 7db21f6cd1484aed802abd7b4349825ac50b98cbb323dcda54aa63ec95ff1e69#1 \
---tx-in 6f0f3152e5cd76b4f22e86767bccad96b22e6bf9abbfa9bcf84b81066156c4ed#0 \
+--tx-in 1078f6a508bde2344bc3bb369d3a0401035c87eb7cbfbc97a8cb3c1e20e843ea#1 \
+--tx-in 1078f6a508bde2344bc3bb369d3a0401035c87eb7cbfbc97a8cb3c1e20e843ea#0 \
 --tx-out addr_test1qr8drf5eur4emcrsanv6ppcf3pywzkzpx3pp2hhr273ddgl34r3hjynmsy2cxpc04a6dkqxcsr29qfl7v9cmrd5mm89q4t0cw0+50000000+"1 f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a.000de140707a636f6e74726163742e30303031" \
---tx-out-reference-script-file contract.plutus \
+--tx-out-reference-script-file ../contract/contract.cbor \
 --change-address addr_test1qr8drf5eur4emcrsanv6ppcf3pywzkzpx3pp2hhr273ddgl34r3hjynmsy2cxpc04a6dkqxcsr29qfl7v9cmrd5mm89q4t0cw0 \
 --invalid-hereafter 25024243 \
 --out-file tx.body
