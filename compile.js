a764e1e56d6b9d56b0b3ae4468c4eb8309ecddd8
papag00se <jlanderson@gmail.com>
2024-05-20 16:00:41 -0700

    Fixing destructure bug aftger adding struct property

--- compile.js
+++ compile.js
@@ -1,13 +1,8 @@
 import * as helios from "./helios.js"
 import fs from "fs";
 const OPTIMIZE = Boolean(process.env.OPTIMIZE || false);
-const REMOVE_ERRORS = Boolean(process.env.REMOVE_ERRORS || false);
 //helios.config.set({IS_TESTNET: false})
 let contractHelios = fs.readFileSync("./contract.helios").toString();
-if (REMOVE_ERRORS) {
-    contractHelios = contractHelios.replace(/assert\(((?:.|\n)*?), ".*?\);/gm, 'assert($1, "");')
-    fs.writeFileSync("./contract/contract_no_asserts.helios", contractHelios);
-}
 let program = helios.Program.new(contractHelios);
 console.log(`OPTIMIZE is set to ${OPTIMIZE}`);
 const contract = program.compile(OPTIMIZE);
@@ -20,13 +15,6 @@ fs.writeFileSync("./contract/contract.cbor", Buffer.from(JSON.parse(contract.ser
 fs.writeFileSync("./contract/contract.addr", address.toBech32());
 fs.writeFileSync("./contract/contract.hash", contract.validatorHash.hex);
 fs.writeFileSync("./contract/contract.uplc", contract.toString());
-
-// // Treasury
-// console.log("Treasury", helios.Address.fromBech32('addr1x92852d60qgsjm9wxsvheerqkrvtshyezcyxula3tgn8h0250g5m57q3p9k2udqe0njxpvxchpwfj9sgdelmzk3x0w7sz9zapd').validatorHash.hex)
-// // Provider
-// console.log("Provider", helios.Address.fromBech32('addr1xysgj7dndz9ql57jsh5y0ss258d0yl8wqfj4hy00ulyw6ueq39umx6y2plfa9p0gglpq4gw67f7wuqn9twg7le7ga4es4uake8').validatorHash.hex)
-// // Settings
-// console.log("Settings", helios.Address.fromBech32('addr1x9lr5j907rwl4hkz98gnle8v238l8nz0q3rzn48rr6p4nur78fy2luxalt0vy2w38ljwc4z070xy7pzx982wx85rt8cqc65x5w').validatorHash.hex)
 
 // IN CLI:
 // helios compile contract.helios --optimize -o contract.json
